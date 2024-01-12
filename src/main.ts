629829261eee8181fe0b78aef4007f92eabcbaef
houchti <play.houchti@gmail.com>
2024-01-12 15:58:49 +0100

    integration Swagger UI et Winston

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,34 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as winston from 'winston';
+import * as DailyRotateFile from 'winston-daily-rotate-file';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  // Configuration de Winston
+  const logger = winston.createLogger({
+    level: 'silly', // Le niveau le plus bas, pour enregistrer tous les niveaux de logs
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json()
+    ),
+    transports: [
+      new DailyRotateFile({
+        filename: 'logs/application-%DATE%.log',
+        datePattern: 'YYYY-MM-DD-HH',
+        zippedArchive: true,
+        maxSize: '20m',
+        maxFiles: '14d'
+      }),
+      new winston.transports.Console({
+        format: winston.format.simple(),
+      }),
+    ],
+  });
+
+  app.useLogger(logger);
+
   await app.listen(3000);
 }
-
 bootstrap();
